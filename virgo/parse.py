84f562c757f43f9656d66a06b7295af633b515d9
Jack Grahl <jack.grahl@gmail.com>
2021-02-21 23:35:14 +0000

    Implement comments.

--- virgo/parse.py
+++ virgo/parse.py
@@ -1,4 +1,15 @@
+import re
 
+
+COMMENT_REGEX = re.compile(r"[/]{2}.*$")
+
+def _gen_preprocess(data):
+    for line in data.split("\n"):
+        yield COMMENT_REGEX.sub(line, "")        
+
+
+def preprocess(data):
+    return "\n".join(_gen_preprocess(data))
 
 
 def parse(data):
