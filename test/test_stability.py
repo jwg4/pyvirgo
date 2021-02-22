0c69bf6cddcab633fccbc85d261ce58938fdc0d4
Jack Grahl <jack.grahl@gmail.com>
2021-02-22 21:11:54 +0000

    Test using a specific alphabet.

--- test/test_stability.py
+++ test/test_stability.py
@@ -5,7 +5,11 @@ from hypothesis import given
 from hypothesis.strategies import text
 
 
-@given(s=text())
+ALPHABET = "abcedfghijklmnopqrstuvwxyz0123456789-<>,\n"
+
+
+#@given(s=text())
+@given(s=text(alphabet=ALPHABET))
 def test_gets_parsed_or_throws_good_error(s):
     try:
         g = virgo.loads(s)
