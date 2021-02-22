6a3b14abdc1fcecdf26f66579b6d648256e484d5
Jack Grahl <jack.grahl@gmail.com>
2021-02-22 02:14:23 +0000

    Direct successors/predecessors.

--- virgo/graph.py
+++ virgo/graph.py
@@ -1,22 +1,32 @@
+from collections import defaultdict
+
+
 class Graph(object):
     def __init__(self):
-        self.nodes = set()
-        self.edges = dict()
+        self.nodes = dict()
+        self.edges = defaultdict(set)
 
     def add_connection(self, connection):
         for n in connection.first:
-            self.nodes.add(n)
+            self.nodes[n] = n
         for n in connection.second:
-            self.nodes.add(n)
+            self.nodes[n] = n
         
         for a in connection.first:
             for b in connection.second:
                 if connection.op == "->" or connection.op == "--":
-                    key = (a, b)
-                    self.edges[key] = True
+                    self.edges[a].add(b)
                 if connection.op == "<-" or connection.op == "--":
-                    key = (b, a)
-                    self.edges[key] = True
+                    self.edges[b].add(a)
+
+    def direct_successors_of(self, node):
+        return self.edges[node]
+
+    def direct_predecessors_of(self, node):
+        for n in self.nodes:
+            if node in self.edges[n]:
+                yield n
+        
                 
          
 def make_graph(connection_list):
