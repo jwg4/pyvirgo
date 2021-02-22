c47271b56a6d0f3f43b097b8d739418d267ea368
Jack Grahl <jack.grahl@gmail.com>
2021-02-22 02:08:37 +0000

    Add more examples as tests.

--- test/test_example.py
+++ test/test_example.py
@@ -6,3 +6,24 @@ def test_parse_basic_example():
         data = f.read()
     g = virgo.parse(data)
     assert g is not None 
+
+
+def test_parse_harder_example():
+    with open("test/files/make.vgo") as f:
+        data = f.read()
+    g = virgo.parse(data)
+    assert g is not None 
+
+
+def test_parse_edge_statement():
+    with open("test/files/edges.vgo") as f:
+        data = f.read()
+    g = virgo.parse(data)
+    assert g is not None 
+
+
+def test_parse_node_statement():
+    with open("test/files/nodes.vgo") as f:
+        data = f.read()
+    g = virgo.parse(data)
+    assert g is not None 
