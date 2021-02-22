fe0a4b03feaabaec08e333678c1ffbc74bcd4dc8
Jack Grahl <jack.grahl@gmail.com>
2021-02-22 02:57:36 +0000

    Add a test for a simple node spec.

--- test/test_parse.py
+++ test/test_parse.py
@@ -14,3 +14,11 @@ def test_parse_simple_edge_with_newline():
     s = "a -> b\n"
     result = parse(s)
     assert result is not None
+
+
+def test_parse_simple_node_description():
+    s = "parser = `goyacc parser.y`"
+    result = parse(s)
+    assert result is not None
+    assert "parser" in result.nodes
+    assert result.nodes["parser"] == "goyacc parser.y"
