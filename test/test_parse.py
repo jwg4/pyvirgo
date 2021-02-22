7c26511b0111dad7e5cd82bbe55c0479f921bb5d
Jack Grahl <jack.grahl@gmail.com>
2021-02-22 01:27:33 +0000

    Check that we have the successors.

--- test/test_parse.py
+++ test/test_parse.py
@@ -6,6 +6,10 @@ def test_parse_simple_edge():
     result = parse(s)
     assert result is not None
     assert "a" in result.nodes
+    assert result.direct_successors_of("a") == ["b"]
+    assert result.successors_of("a") == ["b"]
+    assert result.direct_successors_of("b") == []
+    assert result.successors_of("b") == []
 
 
 def test_parse_simple_edge_with_newline():
